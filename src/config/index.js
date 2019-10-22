4d1b765ddc307d104bc309c9985012b5d4e7d1dc
Diego Meneses <digoxdc99@hotmail.com>
2019-10-22 17:03:35 -0500

    MongoDB connection

--- src/config/index.js
+++ src/config/index.js
@@ -11,5 +11,8 @@ module.exports = {
   port: process.env.PORT || 3000,
   logs: {
     level: process.env.LOG_LEVEL || 'silly'
+  },
+  db: {
+    url: process.env.DB_URL
   }
 }
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,5 +1,7 @@
 const expressLoader = require('./express')
+const mongooseLoader = require('./mongoose')
 
 module.exports = async (app) => {
   await expressLoader(app)
+  await mongooseLoader()
 }
